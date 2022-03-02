a6788186776f9c58e20a428ad7baae15e9c247e4
SparrowDn <kidon.kinn@gmail.com>
2022-03-02 20:33:04 +0900

    new2

---
+++ lib/data/listData.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  String title;
+  int value;
+  Item(this.title, this.value);
+}
+
+class Area {
+  List<DropdownMenuItem<Item>> seoulArea = List.empty(growable:true);
+
+  Area() {
+    seoulArea.add(DropdownMenuItem(child: Text('강남구'), value: Item('강남구', 1),));
+  }
+}
+
+class Kind {
+  List<DropdownMenuItem<Item>> kinds = List.empty(growable: true);
+
+  Kind() {
+    kinds.add(DropdownMenuItem(child: Text('관광지'), value: Item('관광지', 12),));
+  }
+}
